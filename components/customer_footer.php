981e0d1e5e69b1d6bc040e6c67bccbc49aad3a5f
vijayguptagh <120735063+vijayguptagh@users.noreply.github.com>
2023-11-06 04:31:51 +0530

    Add files via upload

---
+++ components/customer_footer.php
@@ -0,0 +1,99 @@
+<!-- 
+		Footer 
+	-->
+	<footer id="footer" class="section footer">
+		<div class="container">
+			<div class="footer__top">
+				<div class="footer-top__box">
+					<h3>Medicines</h3>
+					<a href="#">Trending</a>
+					<a href="#">New</a>
+					<a href="#">Specials</a>
+					<a href="#">Featured</a>
+					<a href="#">Categories</a>
+					<a href="#">Shops</a>
+				</div>
+				<div class="footer-top__box">
+					<h3>INFORMATION</h3>
+					<a href="#">About Us</a>
+					<a href="#">Privacy Policy</a>
+					<a href="#">Terms & Conditions</a>
+					<a href="#">Contact Us</a>
+					<a href="#">Site Map</a>
+				</div>
+				<div class="footer-top__box">
+					<h3>Login</h3>
+					<a href="#">Shopkeeper Login</a>
+					<a href="#">Admin Login</a>
+				</div>
+				<div class="footer-top__box">
+					<h3>CONTACT US</h3>
+					<div>
+						<span>
+							<svg>
+								<use xlink:href="./assets/img/sprite.svg#icon-location"></use>
+							</svg>
+						</span>
+						XXX, ABCD Rd, KLMN city(W), Mumbai-400070
+					</div>
+					<div>
+						<span>
+							<svg>
+								<use xlink:href="./assets/img/sprite.svg#icon-envelop"></use>
+							</svg>
+						</span>
+						abcd@xyz.in
+					</div>
+					<div>
+						<span>
+							<svg>
+								<use xlink:href="./assets/img/sprite.svg#icon-phone"></use>
+							</svg>
+						</span>
+						022 6887 8730
+					</div>
+					<div>
+						<span>
+							<svg>
+								<use xlink:href="./assets/img/sprite.svg#icon-paperplane"></use>
+							</svg>
+						</span>
+						pqrst city, Mumbai
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer__bottom">
+			<div class="footer-bottom__box">
+
+			</div>
+			<div class="footer-bottom__box">
+
+			</div>
+		</div>
+		</div>
+	</footer>
+	<!-- End Footer -->
+
+
+	<!-- Go To -->
+
+	<a href="#header" class="goto-top scroll-link">
+		<svg>
+			<use xlink:href="./assets/img/sprite.svg#icon-arrow-up"></use>
+		</svg>
+	</a>
+
+
+	<!-- Glide Carousel Script -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.4.1/glide.min.js"></script>
+	<!-- Animate On Scroll -->
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+	<!-- Custom JavaScript -->
+	<script src="./assets/js/index.js"></script>
+	<script src="./assets/js/slider.js"></script>
+
+</body>
+
+</html>
