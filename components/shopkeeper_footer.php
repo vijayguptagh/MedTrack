4516539cd021b2338a928ce5f440ea9ae80f89b3
vijayguptagh <120735063+vijayguptagh@users.noreply.github.com>
2024-08-04 10:02:12 +0530

    Update shopkeeper_footer.php

--- components/shopkeeper_footer.php
+++ components/shopkeeper_footer.php
@@ -1,4 +1,9 @@
 <!-- ======= Footer ======= -->
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+			class="bi bi-arrow-up-short"></i></a>
+
+	<!-- Vendor JS Files -->
+	<script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="../assets/js/admin_script.js"></script>
 <footer id="footer" class="footer">
 		<div class="copyright">
@@ -10,11 +15,7 @@
 	</footer>
 	<!-- End Footer -->
 
-	<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-			class="bi bi-arrow-up-short"></i></a>
-
-	<!-- Vendor JS Files -->
-	<script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	
 	
     
 </body>
